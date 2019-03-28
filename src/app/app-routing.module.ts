546d0c93067eed2409366cd911cce7bd6d7b5355
NAYLA KATTYA PACHECO CHURA <V8360317@alunos.etec.itaqua>
2019-03-27 22:31:55 -0300

    criei um botão e crii uma pagina nova

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'novo-plano', loadChildren: './novo-plano/novo-plano.module#NovoPlanoPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/novo-plano/novo-plano.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-novo-plano',
+  templateUrl: './novo-plano.page.html',
+  styleUrls: ['./novo-plano.page.scss'],
+})
+export class NovoPlanoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
