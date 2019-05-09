8976399961ad02b478868f6c2f4581fefca4b73c
NAYLA KATTYA PACHECO CHURA <V8360317@alunos.etec.itaqua>
2019-05-08 22:34:18 -0300

    implementação do sessionstorage para cadastro de novos planos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  planejamentos = []
+  ionViewDidEnter(){
+    this.planejamentos = []
+    const tamanhoDoBanco = sessionStorage.length
+    for (let index = 0; index < tamanhoDoBanco; index++) {
+      const chave = sessionStorage.key(index)
+      if(chave !== 'ionic-persist-config'){
+      const planejamento = sessionStorage.getItem(chave)
+      this.planejamentos.push(JSON.parse(planejamento))
+      }
+      
+    }
+  }
 
 }
--- src/app/novo-plano/novo-plano.page.ts
+++ src/app/novo-plano/novo-plano.page.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./novo-plano.page.scss'],
 })
 export class NovoPlanoPage implements OnInit {
+erro;
+  constructor() { 
 
-  constructor() { }
+  }
 
   ngOnInit() {
+
+  }
+  calcular(form){
+    const nomeDoPlano = form.value.nome
+    const custoMensal = form.value.custoMensal
+    const faturamentoMensal = form.value.faturamentoMensal
+
+    if(parseInt(custoMensal) > parseInt(faturamentoMensal)){
+      this.erro = "Seu custo mensal é maior que seu faturamento."
+    } else {
+      sessionStorage.setItem(nomeDoPlano, JSON.stringify(form.value))
+      this.erro=" Cadastrado com sucesso!"
+      form.reset()
+    }
+   
+    
   }
 
 }
