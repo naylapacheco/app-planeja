59751e57b773052c97804ef1ebed622fd9d51046
NAYLA KATTYA PACHECO CHURA <V8360317@alunos.etec.itaqua>
2019-05-15 22:33:48 -0300

    fizemos o botão excluir

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private alert: AlertController){
+
+  }
   planejamentos = []
-  ionViewDidEnter(){
+
+  listar (){
     this.planejamentos = []
     const tamanhoDoBanco = sessionStorage.length
     for (let index = 0; index < tamanhoDoBanco; index++) {
@@ -18,6 +24,30 @@ export class HomePage {
       }
       
     }
+
+  }
+  //await força o fluxo a parar ...create é um metodo , alertTemp é minha variavel//
+  async exibirAlertaDeExclusao () {
+    const alertTemp = await this.alert.create({
+      header: 'Exclusão de plano',
+      subHeader:'Deseja realmente excluir esse plano?',
+    message:''
+    })
+    await alertTemp.present()
+
+  }
+  excluir(nomeDoPlano){
+    this.exibirAlertaDeExclusao()
+    //sessionStorage.removeItem(nomeDoPlano)
+    //this.listar()
+    
+
+  }
+
+  //carrega primeiro,executa tudo de novo: ionViewDidEnter//
+  ionViewDidEnter(){
+    this.listar()
+   
   }
 
 }
--- src/app/novo-plano/novo-plano.page.ts
+++ src/app/novo-plano/novo-plano.page.ts
@@ -1,30 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-novo-plano',
   templateUrl: './novo-plano.page.html',
   styleUrls: ['./novo-plano.page.scss'],
 })
-export class NovoPlanoPage implements OnInit {
+export class NovoPlanoPage {
+  //nova classe dentro //
+  constructor(private nav:NavController){
+
+  }
 erro;
-  constructor() { 
-
-  }
-
-  ngOnInit() {
-
-  }
+ 
   calcular(form){
     const nomeDoPlano = form.value.nome
     const custoMensal = form.value.custoMensal
     const faturamentoMensal = form.value.faturamentoMensal
+    const custoMensalConvertido = parseInt(custoMensal)
+    const faturamentoMensalConvertido = parseInt(faturamentoMensal)
 
-    if(parseInt(custoMensal) > parseInt(faturamentoMensal)){
+    if( custoMensalConvertido > faturamentoMensalConvertido ){
       this.erro = "Seu custo mensal é maior que seu faturamento."
     } else {
       sessionStorage.setItem(nomeDoPlano, JSON.stringify(form.value))
-      this.erro=" Cadastrado com sucesso!"
       form.reset()
+      //volta a pagina inicial//
+      this.nav.back()
     }
    
     
